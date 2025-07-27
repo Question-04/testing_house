2ee5e0951d68c9e9eac0d7215c99a5cff24f49e4
Question-04 <alok8official8@gmail.com>
2025-07-27 08:09:40 +0530

    Optimize repository size by removing large build files and image collections

--- public/collection/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Hhx10EjPZEtpwopXHTBmV8LuL/jhGfuVcIt3wusAgNs=\"\n}"
--- public/collection/.next/static/chunks/src_app_page_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_0766a282._.js",
-  "static/chunks/src_app_components_91c9f11f._.js",
-  "static/chunks/src_app_components_bf037b38._.css"
-],
-    source: "dynamic"
-});
