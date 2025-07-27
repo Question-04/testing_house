cf341b6d8ce50e79357a26960c84a9d1ca33e0c8
Question-04 <alok8official8@gmail.com>
2025-07-27 11:51:23 +0530

    Fix Next.js domain limit: Replace domains array with remotePatterns to support unlimited domains

--- next.config.ts
+++ next.config.ts
@@ -16,87 +16,327 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: [
-      'localhost',
-      '192.168.11.109',
-      'ep-wandering-pine-a8x2p4zl-pooler.eastus2.azure.neon.tech',
-      'houseofplutus.mos.ap-southeast-2.sufybkt.com',
-      'luxurysouq.com',
-      'allsaints.hk',
-      'assets.ajio.com',
-      'assets.christiandior.com',
-      'bta.scene7.com',
-      'ca.stanley1913.com',
-      'cdn-img.poizonapp.com',
-      'cdn.culture-circle.com',
-      'cdn.findyourkicks.com',
-      'cdn.modesens.com',
-      'cdn.shopify.com',
-      'cdna.lystit.com',
-      'crepdogcrew.com',
-      'culture-cirlce-static-dge7g8b8eue6fvcv.z02.azurefd.net',
-      'culturecirclestorage.blob.core.windows.net',
-      'd2cva83hdk3bwc.cloudfront.net',
-      'dawntown.co.in',
-      'djm0962033frr.cloudfront.net',
-      'dripproject.co',
-      'encrypted-tbn0.gstatic.com',
-      'encrypted-tbn1.gstatic.com',
-      'freesociety.in',
-      'henneybear.in',
-      'hm8store.com',
-      'hrd-live.cdn.scayle.cloud',
-      'hypeclub.net',
-      'hypefly.co.in',
-      'i.ebayimg.com',
-      'i.pinimg.com',
-      'image-cdn.hypb.st',
-      'image.goat.com',
-      'images-cdn.ubuy.co.in',
-      'images.asos-media.com',
-      'images.stockx.com',
-      'img.shopstyle-cdn.com',
-      'img.stadiumgoods.com',
-      'javisneaker.com',
-      'kalakaarindiaa.com',
-      'kidspark.co.in',
-      'luxecollectivefashion.com',
-      'm.media-amazon.com',
-      'madisonavenuecouture.com',
-      'makeshop-multi-images.akamaized.net',
-      'marais.com.au',
-      'marketplace.mainstreet.co.in',
-      'media-assets.grailed.com',
-      'media.endclothing.com',
-      'nubiantokyo.com',
-      'odto.com',
-      'offkicksinc.com',
-      'ormoda.com',
-      'res.cloudinary.com',
-      'sneakerplug.co.in',
-      'static.flexdog.com',
-      'stayfresh.ca',
-      'storage.googleapis.com',
-      'thefactorykl.com',
-      'thehouseofdrew.com',
-      'underratedstore.com',
-      'unmatchedkicks.in',
-      'viaanakidsstore.com',
-      'www.allsaints.com',
-      'www.copunderdog.com',
-      'www.crepslocker.com',
-      'www.culture-circle.com',
-      'www.fridaycharm.com',
-      'www.hypeelixir.com',
-      'www.hyperyno.com',
-      'www.kicksmachine.com',
-      'www.net-a-porter.com',
-      'www.oakshop.ca',
-      'www.outlined.ca',
-      'www.pixelcut.ai',
-      'www.stanley1913.com',
-      'www.stanley1913.com.br',
-      'www.thebckspace.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: '192.168.11.109',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ep-wandering-pine-a8x2p4zl-pooler.eastus2.azure.neon.tech',
+      },
+      {
+        protocol: 'https',
+        hostname: 'houseofplutus.mos.ap-southeast-2.sufybkt.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'luxurysouq.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'allsaints.hk',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.ajio.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.christiandior.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bta.scene7.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ca.stanley1913.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-img.poizonapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.culture-circle.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.findyourkicks.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.modesens.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdna.lystit.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'crepdogcrew.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'culture-cirlce-static-dge7g8b8eue6fvcv.z02.azurefd.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'culturecirclestorage.blob.core.windows.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd2cva83hdk3bwc.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dawntown.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'djm0962033frr.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dripproject.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn1.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'freesociety.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'henneybear.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hm8store.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hrd-live.cdn.scayle.cloud',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hypeclub.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hypefly.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ebayimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cdn.hypb.st',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.goat.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-cdn.ubuy.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.asos-media.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.stockx.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.shopstyle-cdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.stadiumgoods.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'javisneaker.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kalakaarindiaa.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kidspark.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'luxecollectivefashion.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'madisonavenuecouture.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'makeshop-multi-images.akamaized.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'marais.com.au',
+      },
+      {
+        protocol: 'https',
+        hostname: 'marketplace.mainstreet.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media-assets.grailed.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.endclothing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nubiantokyo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'odto.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'offkicksinc.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ormoda.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sneakerplug.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.flexdog.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'stayfresh.ca',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'thefactorykl.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'thehouseofdrew.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'underratedstore.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unmatchedkicks.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'viaanakidsstore.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.allsaints.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.copunderdog.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.crepslocker.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.culture-circle.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.fridaycharm.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.hypeelixir.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.hyperyno.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kicksmachine.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.net-a-porter.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.oakshop.ca',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.outlined.ca',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pixelcut.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.stanley1913.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.stanley1913.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.thebckspace.com',
+      },
     ],
   },
 };
