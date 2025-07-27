47d3869a0ec71bae72943ee6248b0d1a3809c375
Question-04 <122510061+Question-04@users.noreply.github.com>
2025-07-27 16:05:04 +0530

    Update server.go

--- plutus-backend/server.go
+++ plutus-backend/server.go
@@ -255,11 +255,9 @@ func corsHandlerFunc(h http.HandlerFunc) http.Handler {
 		AllowedOrigins: []string{
 			"*", // Allow all origins
 			"https://testing2-blush-ten.vercel.app",
-			"https://final-test-tan.vercel.app",
-			"https://testing-house.onrender.com",
-			"http://localhost:3000",
-			"http://localhost:3001",
-			"http://localhost:8090",
+			
+	
+		
 		},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
